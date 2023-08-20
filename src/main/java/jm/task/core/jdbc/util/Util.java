a72852cf4c33958f372d4acc168a59d3dc6c51ae
Alexei <splash.dll@gmail.com>
2023-08-20 12:08:15 +0300

    refactoring

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -2,10 +2,7 @@ package jm.task.core.jdbc.util;
 
 import jm.task.core.jdbc.model.User;
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.cfg.Environment;
-import org.hibernate.service.ServiceRegistry;
 
 
 import java.io.IOException;
--- src/test/java/UserServiceTest.java
+++ src/test/java/UserServiceTest.java
@@ -1,7 +1,6 @@
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
-import jm.task.core.jdbc.service.UserServiceImplOLD;
 import org.junit.Assert;
 import org.junit.Test;
 
